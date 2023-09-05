011c8a4dba48e0caa01bd10551374d6477dae2c9
8200118 <58340504+heyliceeee@users.noreply.github.com>
2023-09-05 12:37:01 +0100

    :sparkles: build: componente contentor
    - altera classes que vai mudar a disposição dos itens que estão dentro do contentor

--- costs/src/App.js
+++ costs/src/App.js
@@ -1,8 +1,11 @@
 import {BrowserRouter as Router, Route, Routes, Link} from 'react-router-dom';
+
 import Home from './components/pages/Home';
 import Company from './components/pages/Company';
 import ContactUs from './components/pages/ContactUs';
 import NewProject from './components/pages/NewProject';
+
+import Container from './components/layout/Container';
 
 
 function App() {
@@ -15,12 +18,14 @@ function App() {
         <Link to="/newproject">New Project</Link>
       </ul>
 
-      <Routes>
-        <Route exact path='/' element={<Home/>}/>
-        <Route exact path='/company' element={<Company/>}/>
-        <Route exact path='/contactus' element={<ContactUs/>}/>
-        <Route exact path='/newproject' element={<NewProject/>}/>
-      </Routes>
+      <Container customClass="min-height">
+        <Routes>
+          <Route exact path='/' element={<Home/>}/>
+          <Route exact path='/company' element={<Company/>}/>
+          <Route exact path='/contactus' element={<ContactUs/>}/>
+          <Route exact path='/newproject' element={<NewProject/>}/>
+        </Routes>
+      </Container>
 
       <p>Footer</p>
     </Router>
