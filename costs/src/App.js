a12ac54c9c9f02c14bb3be51273ee9da53e0746d
8200118 <58340504+heyliceeee@users.noreply.github.com>
2023-09-05 12:06:56 +0100

    :sparkles: página inicial - rotas

--- costs/src/App.js
+++ costs/src/App.js
@@ -1,9 +1,19 @@
+import {BrowserRouter as Router, Switch, Route, Routes} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <p>Costs</p>
-    </div>
+    <Router>
+      <p>Navbar</p>
+
+      <Routes>
+        <Route exact path='/' element={<Home/>}/>
+        <Route path='/company' element={<Company/>}/>
+        <Route path='/contactus' element={<ContactUs/>}/>
+        <Route path='/newproject' element={<NewProject/>}/>
+      </Routes>
+
+      <p>Footer</p>
+    </Router>
   );
 }
 
