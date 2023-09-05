39fb1c55bbbb77a647ef2dbbcec2c28aef09e19e
8200118 <58340504+heyliceeee@users.noreply.github.com>
2023-09-05 12:17:45 +0100

    :sparkles: página inicial - rotas e páginas

--- costs/src/App.js
+++ costs/src/App.js
@@ -1,15 +1,25 @@
-import {BrowserRouter as Router, Switch, Route, Routes} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Routes, Link} from 'react-router-dom';
+import Home from './components/pages/Home';
+import Company from './components/pages/Company';
+import ContactUs from './components/pages/ContactUs';
+import NewProject from './components/pages/NewProject';
+
 
 function App() {
   return (
     <Router>
-      <p>Navbar</p>
+      <ul>
+        <Link to="/">Home</Link>
+        <Link to="/company">Company</Link>
+        <Link to="/contactus">Contact Us</Link>
+        <Link to="/newproject">New Project</Link>
+      </ul>
 
       <Routes>
         <Route exact path='/' element={<Home/>}/>
-        <Route path='/company' element={<Company/>}/>
-        <Route path='/contactus' element={<ContactUs/>}/>
-        <Route path='/newproject' element={<NewProject/>}/>
+        <Route exact path='/company' element={<Company/>}/>
+        <Route exact path='/contactus' element={<ContactUs/>}/>
+        <Route exact path='/newproject' element={<NewProject/>}/>
       </Routes>
 
       <p>Footer</p>
