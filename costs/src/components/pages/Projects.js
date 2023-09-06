c182ccbd438fda8ff270b9c95ceb3fa9281f8123
8200118 <58340504+heyliceeee@users.noreply.github.com>
2023-09-06 13:43:13 +0100

    :lipstick: feat: Estilização CSS da página meus projetos
    - dashboard

--- costs/src/components/pages/Projects.js
+++ costs/src/components/pages/Projects.js
@@ -1,6 +1,10 @@
-import Message from "../layout/Message";
+import styles from './Projects.module.css';
 
 import { useLocation } from "react-router-dom";
+
+import Message from "../layout/Message";
+import Container from "../layout/Container";
+import LinkButton from "../layout/LinkButton";
 
 function Projects() {
     //ler o que está no newProject
@@ -13,9 +17,17 @@ function Projects() {
     }
 
     return (
-        <div>
-            <h1>My Projects</h1>
+        <div className={styles.project_container}>
+            <div className={styles.title_container}>
+                <h1>My Projects</h1>
+                <LinkButton to="/newproject" text="Create project"/>
+            </div>
+
             {message && <Message type="success" msg={message}/>}
+
+            <Container customClass="start">
+                <p>Projects...</p>
+            </Container>
         </div>
     );
 }
