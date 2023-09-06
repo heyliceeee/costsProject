331ebbad6299e0d8de461668b0d36f8a16c98c72
8200118 <58340504+heyliceeee@users.noreply.github.com>
2023-09-06 14:36:32 +0100

    :sparkles: feat: mostrar dados via API

--- costs/src/components/pages/Projects.js
+++ costs/src/components/pages/Projects.js
@@ -1,15 +1,35 @@
 import styles from './Projects.module.css';
 
 import { useLocation } from "react-router-dom";
+import { useState, useEffect } from 'react';
 
 import Message from "../layout/Message";
 import Container from "../layout/Container";
 import LinkButton from "../layout/LinkButton";
+import ProjectCard from '../project/ProjectCard';
+
 
 function Projects() {
+    
+    const [projects, setProjects] = useState([]);//guardar os projetos
+
     //ler o que está no newProject
     const location = useLocation();
     let message = '';
+
+    useEffect(() => {
+        fetch('http://localhost:5000/projects', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        }).then((resp) => resp.json())
+        .then((data) => {
+            console.log(data);
+            setProjects(data)
+        })
+        .catch((err) => console.log(err))
+    }, []);
 
     //se tiver alguma coisa no state, vou verificar se a message existe
     if(location.state){
@@ -26,7 +46,10 @@ function Projects() {
             {message && <Message type="success" msg={message}/>}
 
             <Container customClass="start">
-                <p>Projects...</p>
+                {projects.length > 0 && 
+                projects.map((project) => 
+                    <ProjectCard id={project.id} name={project.name} budget={project.budget} category={project.category.name} key={project.id}/>
+                )}
             </Container>
         </div>
     );
---
+++ costs/src/components/project/ProjectCard.js
@@ -0,0 +1,20 @@
+import styles from './ProjectCard.module.css';
+
+import { BsPencil, BsFillTrashFill } from 'react-icons/bs';
+
+function ProjectCard({id, name, budget, category, handleRemove}) {
+    return (
+        <div>
+            <h4>{name}</h4>
+            <p><span>Budget:</span> {budget}€</p>
+            <p><span></span> {category}</p>
+
+            <div>
+                <p>Edit</p>
+                <p>Delete</p>
+            </div>
+        </div>
+    );
+}
+
+export default ProjectCard;
