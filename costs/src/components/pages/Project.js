88bdc40771312dec5ab32821e20c41dcca5ee16d
8200118 <58340504+heyliceeee@users.noreply.github.com>
2023-09-06 20:00:20 +0100

    :lipstick: feat: Estilização CSS da página meus projetos - editar projeto
    - formulario de edição do projeto

--- costs/src/components/pages/Project.js
+++ costs/src/components/pages/Project.js
@@ -5,12 +5,16 @@ import { useState, useEffect } from 'react';
 
 import Loading from './../layout/Loading';
 import Container from './../layout/Container';
+import ProjectForm from './../project/ProjectForm';
+import Message from './../layout/Message';
 
 function Project() {
 
     const { id } = useParams(); //passar o id do projeto para esta página
     const [project, setProject] = useState([]);
     const [showProjectForm, setShowProjectForm] = useState(false);
+    const [message, setMessage] = useState();
+    const [type, setType] = useState();
 
     useEffect(() => {
         setTimeout(() => {
@@ -34,12 +38,39 @@ function Project() {
     }
 
 
+    function editProject(project){
+        //budget validation
+
+        if(project.budget < project.cost){
+           setMessage('The budget cannot be less than the project cost!');
+           setType('error');
+
+           return false;
+        }
+
+        fetch(`http://localhost:5000/projects/${project.id}`, {
+            method: 'PATCH',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(project)
+        }).then((resp) => resp.json())
+        .then((data) => {
+            setProject(data);
+            setShowProjectForm(false);
+            
+            setMessage('Project updated!');
+            setType('success');
+        })
+        .catch((err) => console.log(err));
+    }
+
+
     return (
         <>
         {project.name ? (
 
         <div className={styles.project_details}>
             <Container customClass="column">
+                {message && <Message type={type} msg={message}/>}
                 <div className={styles.details_container}>
                     <h1>Project: {project.name}</h1>
                     <button className={styles.btn} onClick={toggleProjectForm}> {/* //se não mostra o form mostra o botão 'edit project', se o form tiver a mostra mostra o botão 'close' */}
@@ -59,7 +90,7 @@ function Project() {
                     </div>) 
                     : 
                     (<div className={styles.project_info}>
-                        <p>form</p>
+                        <ProjectForm handleSubmit={editProject} btnText="Save changes" projectData={project}/>
                     </div>)
                     }
                 </div>
